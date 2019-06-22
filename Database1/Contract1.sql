1e19e28d742983f0d670246deb21c7a670bd7218
ppp <o_b467@mail.ru>
2019-06-22 18:22:15 +0300

    1

---
+++ Database1/Contract1.sql
@@ -0,0 +1,5 @@
+﻿CREATE CONTRACT [Contract1]
+(
+	[mess1] SENT BY INITIATOR,
+	[mess2] SENT BY TARGET
+)
---
+++ Database1/SqlStoredProcedure1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class StoredProcedures
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void SqlStoredProcedure1()
+    {
+        using (SqlConnection connection = new SqlConnection("context connection=true"))
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from table1", connection);
+            SqlDataReader r = command.ExecuteReader();
+            SqlContext.Pipe.Send(r);
+        }
+
+    }
+}
